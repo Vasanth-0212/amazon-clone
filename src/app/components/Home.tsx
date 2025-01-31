554a7b304bafe0b69eb542478bb2b71c7c1a0d28
Vasanth-0212 <vasanthathuluri02@gmail.com>
2025-01-31 22:35:29 +0530

    fixed images issue

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -42,28 +42,28 @@ const Home = () => {
                 <img src="/home3.png" style={{ width: "100%" }} />
             </div>
             <div className="absolute bottom-[-200px] flex flex-wrap justify-between w-screen px-[50px]">
-                <div className="h-[440px] w-[350px] bg-white mx-1 border border-gray-300">
+                <div className="h-[420px] w-[320px] bg-white mx-1 rounded-md border border-gray-300">
                     <div className="flex flex-col items-center">
                         <h1 className="text-2xl font-semibold p-5">Crafting treasures from tradition</h1>
                         <img src="/crafts.png" className="h-[250px] w-[300px]" />
                     </div>
                     <p className="text-blue-500 p-6 hover:cursor-pointer">Explore more</p>
                 </div>
-                <div className="h-[440px] w-[325px] bg-white mx-1 border border-gray-300">
+                <div className="h-[420px] w-[320px] bg-white mx-1 rounded-md border border-gray-300">
                     <div className="flex flex-col items-center">
                         <h1 className="text-2xl font-semibold p-5">Collections across small businesses</h1>
                         <img src="/business.png" className="h-[250px] w-[300px]" />
                     </div>
                     <p className="text-blue-500 p-6 hover:cursor-pointer">Explore more</p>
                 </div>
-                <div className="h-[440px] w-[325px] bg-white mx-1 border border-gray-300">
+                <div className="h-[420px] w-[320px] bg-white mx-1 rounded-md border border-gray-300">
                     <div className="flex flex-col items-center">
                         <h1 className="text-2xl font-semibold p-5">Discover new tales of travel across Bharat</h1>
                         <img src="/travel.png" className="h-[250px] w-[300px]" />
                     </div>
                     <p className="text-blue-500 p-6 hover:cursor-pointer">Explore more</p>
                 </div>
-                <div className="h-[440px] w-[325px] bg-white mx-1 border border-gray-300">
+                <div className="h-[420px] w-[320px] bg-white mx-1 rounded-md border border-gray-300">
                     <div className="flex flex-col items-center">
                         <h1 className="text-2xl font-semibold p-5">Discovering the gems of health and healing</h1>
                         <img src="/health.png" className="h-[250px] w-[300px]" />
